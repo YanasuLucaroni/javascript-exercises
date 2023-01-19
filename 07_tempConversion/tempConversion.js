7703d868d4a386e5379bf91276ec7aa56430d10d
YanasuLucaroni <81329768+YanasuLucaroni@users.noreply.github.com>
2023-01-19 15:24:54 -0300

    Ejercicio 7 terminado

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (farenheit) {
+  // (farenheit - 32) * (5 / 9) Esta es la formula para cambiar
+  // farenheit a celsius y después se redondea
+  return Math.round((farenheit - 32) * (5 / 9) * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (celsius) {
+  // celsius * (9 / 5) + 32) Esta es la formula para cambiar
+  // de celsius a farenheit y después se redondea
+  return Math.round((celsius * (9 / 5) + 32) * 10) / 10;
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
