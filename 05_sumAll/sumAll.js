d057acfe5eb9e2c9b5c3b5c00f0fa0b0f5e557fb
YanasuLucaroni <81329768+YanasuLucaroni@users.noreply.github.com>
2023-01-19 13:24:45 -0300

    Ejercicio 5 terminado

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
+const sumAll = function (num1, num2) {
+  //   let sum = 0;
 
+  //   for (let i = num1; i <= num2; i++) {
+  //     sum += i;
+  //   }
+  //   return sum;
+  if (
+    typeof num1 != "number" ||
+    typeof num2 != "number" ||
+    num1 < 0 ||
+    num2 < 0
+  ) {
+    return "ERROR";
+  }
+  let terms;
+  if (num2 < num1) {
+    terms = num1 - num2 + 1;
+  } else {
+    terms = num2 - num1 + 1;
+  }
+  return (terms * (num1 + num2)) / 2;
 };
 
 // Do not edit below this line
