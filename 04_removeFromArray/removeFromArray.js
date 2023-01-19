603f015e53188a3b92436a65f7b11531ed502109
YanasuLucaroni <81329768+YanasuLucaroni@users.noreply.github.com>
2023-01-19 12:53:03 -0300

    Ejercicio 4 terminado

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (array, ...args) {
+  let index;
+  for (let i = 0; i < args.length; i++) {
+    index = array.indexOf(args[i]);
+    if (index != -1) {
+      array.splice(index, 1);
+    }
+  }
+  return array;
 };
 
 // Do not edit below this line
